2a4439623ec8312287cc6e3bc4dc58bb32c14653
Dhruvmodi418 <125945328+Dhruvmodi418@users.noreply.github.com>
2026-02-13 22:22:37 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+let arr = [0, 1, 23, 89, 45, 42, 15]
+
+console.log(arr.length)
+console.log(arr[2]);
+console.log(arr)
+
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+a1=[2,5,3,2,2,2,"This is array1 "]
+a2=[2,5,3,2,2,2,"This is array2 "]
+a3=[]
+
+
+console.log(a2.sort())
+
+console.log(a1.toString())
